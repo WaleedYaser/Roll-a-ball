c0516ab5e5993abf7f36f893d6b3bd0f07f570a1
Waleed Yaser <waleedyaser95@gmail.com>
2018-04-19 04:58:28 +0200

    finish the tutorial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	private int score;
+
+	public float speed = 10;
+	public Text scoreText;
+	public Text winText;
+	public int totalPickups = 8;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		score = 0;
+		SetScoreText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate ()
+	{
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		Vector3 f = new Vector3 (h, 0, v);
+
+		rb.AddForce (f * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+			score += 1;
+			SetScoreText ();
+		}
+	}
+
+	void SetScoreText()
+	{
+		scoreText.text = "Score: " + score.ToString ();
+		if (score >= totalPickups) {
+			winText.text = "You Win!";
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Roll-a-ball
 Unity tutorial Roll-a-ball https://unity3d.com/learn/tutorials/s/roll-ball-tutorial
+
+![Alt Text](GIF/RollABall.gif)
